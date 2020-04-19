9765d4426d2346517bf3e05f3b6e50a4c6997206
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-04-19 19:54:02 +0900

    make repor

--- lecture_1/main.py
+++ lecture_1/main.py
@@ -3,86 +3,107 @@ import numpy as np
 from PIL import Image
 
 
-def get_image_data(path):
-    im = Image.open(path)
-    rgb = np.array(im)
-    return im, rgb
+class ImageProcessing:
+    def __init__(self, path):
+        self.im, self.rgb = self.get_image_data(path)
+        self.show(self.rgb, 'normal', 255)
 
-def show_image(data):
-    pilImg = Image.fromarray(np.uint8(data))
-    pilImg.show()
-
-def compression_data_x(data, scale):
-    ans = []
-    for row in rgb:
-        tmp_row = np.array([[0, 0, 0]])
-        count = 0
-        sum_col = np.zeros(3)
-        for col in row:
-            sum_col += col
-            if count == scale:
-                tmp_row = np.append(tmp_row, np.array([sum_col/scale]), axis=0)
-                count = 0
-                sum_col = np.zeros(3)
-            count += 1
-        ans.append(tmp_row[1:])
-    # show_image(np.array(ans))
-    return np.array(ans)
-
-def compression_data_y(data, scale):
-    ans = []
-    height, width = data.shape[0], data.shape[1]
-    print(height, width)
-    count = 0
-    tmp = np.array([np.array([0, 0, 0], dtype=np.float64) for i in range(width)], dtype=np.float64)
-    for index in range(height):
-        if count == scale:
-            ans.append(tmp / scale)
-            # 初期化
-            tmp = np.array([np.array([0, 0, 0], dtype=np.float64) for i in range(width)], dtype=np.float64)
+    def get_image_data(self, path):
+        im = Image.open(path)
+        rgb = np.array(im)
+        return im, rgb
+    
+    def show(self, data, type, scale):
+        pilImg = Image.fromarray(np.uint8(data))
+        pilImg.show()
+        pilImg.save('./{0}/{0}_{1}.jpg'.format(type, scale))
+        return pilImg
+    
+    # 拡大
+    def get_big_data(self, scale):
+        data = self.rgb.repeat(scale, axis=0).repeat(scale, axis=1)
+        self.show(data, 'big', scale)
+        return data
+    
+    """
+    x軸方向に縮小
+    """
+    def compression_data_x(self, scale):
+        ans = []
+        for row in self.rgb:
+            tmp_row = np.array([[0, 0, 0]])
             count = 0
-        else:
-            tmp = tmp + data[index]
-        count += 1
-    show_image(np.array(ans))
-    return np.array(ans)
+            sum_col = np.zeros(3)
+            for col in row:
+                sum_col += col
+                if count == scale:
+                    tmp_row = np.append(tmp_row, np.array([sum_col/scale]), axis=0)
+                    count = 0
+                    sum_col = np.zeros(3)
+                count += 1
+            ans.append(tmp_row[1:])
+        return np.array(ans)
 
-# 縮小
-def get_small_data(rgb, scale, average=False):
-    if average:
-        if scale == 1:
-            return rgb
-        else:
-            # 横に圧縮
-            ans = compression_data_x(rgb, scale)
-            # 縦に圧縮
-            ans = compression_data_y(ans, scale)
-            return ans
-    else:
-      return rgb[::scale, ::scale]
-
-# 拡大
-def get_big_data(rgb, scale, average=False):
-    pass
-
-# 量子化
-def make_quantization(signal, bit_length):
-    quantized_signal = signal * ((2 ** (bit_length / 2)) - 1) # プラスで8bit，マイナスで8bit
-    quantized_signal = np.floor(quantized_signal) # 小数部切り捨て
-    quantized_signal = quantized_signal / np.max(np.abs(quantized_signal)) # [-1, 1]の区間に
-    return quantized_signal
-
-if __name__ == '__main__':
-    path = './image.jpg'
-    im, rgb = get_image_data(path)
-    width, height = im.size
-    scale = 1
-    small_data = get_small_data(rgb, scale=scale, average=True)
-    pilImg = Image.fromarray(np.uint8(small_data))
-    pilImg.save('image_small_{}.jpg'.format(scale))
-    # pilImg.show()
+    """
+    y軸方向に縮小
+    """
+    def compression_data_y(self, data, scale):
+        ans = []
+        height, width = data.shape[0], data.shape[1]
+        print(height, width)
+        count = 0
+        tmp = np.array([np.array([0, 0, 0], dtype=np.float64) for i in range(width)], dtype=np.float64)
+        for index in range(height):
+            if count == scale:
+                ans.append(tmp / scale)
+                # 初期化
+                tmp = np.array([np.array([0, 0, 0], dtype=np.float64) for i in range(width)], dtype=np.float64)
+                count = 0
+            else:
+                tmp = tmp + data[index]
+            count += 1
+        return np.array(ans)
 
     # 縮小
-    # width_small, height_small = width // 2, height // 2
-    # im_small = im.resize(size=(width_small, height_small))
-    # im_small.save('image_small.jpg')
+    def get_small_data(self, scale, average=False):
+        if average:
+            if scale == 1:
+                self.show(self.rgb, 'small', scale)
+                return rgb
+            else:
+                # 横に圧縮
+                ans = self.compression_data_x(scale)
+                # 縦に圧縮
+                data = self.compression_data_y(ans, scale)
+                self.show(data, 'small', scale)
+                return data
+        else:
+            # 間引く方法
+            data = self.rgb[::scale, ::scale]
+            self.show(data, 'small', -1)
+            return data
+
+    # 量子化
+    def get_quantized_data(self, bit_length):
+        q = 255 / (2 ** bit_length)
+        data = q * np.round(self.rgb / q)
+        self.show(data, 'quantized', bit_length)
+        return data
+
+if __name__ == '__main__':
+      # path = './image.jpg'
+      path = './IE.jpeg'
+      image_processing = ImageProcessing(path)
+      
+      # big_scale = 3
+      # image_processing.get_big_data(big_scale)
+
+      bit_length = 1
+      # bit_length = 2
+      # bit_length = 3
+      # bit_length = 4
+      # image_processing.get_quantized_data(bit_length)
+
+      bit_length = 2
+      average = True
+      image_processing.get_small_data(bit_length, average)
--- lecture_1/test.py
+++ lecture_1/test.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+twice = lambda x: x * 2
+
 if __name__ == '__main__':
     array = np.array([[[1 * i, 2 * i, 3 * i] for i in range(3)] for _ in range(2)])
+    print('-'*10)
     array1 = np.array([[[1 * i, 2 * i, 3 * i] for i in range(3)] for _ in range(2)])
     array += array1
     print(array)
